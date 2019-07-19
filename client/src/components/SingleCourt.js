18ad5c1ee565d3466eb14666014c73f13e4f94f0
JordanAclarke <JordanAclarke@gmail.com>
2019-07-19 14:06:54 -0400

    form issues

--- client/src/components/SingleCourt.js
+++ client/src/components/SingleCourt.js
@@ -5,7 +5,7 @@ export default class SingleCourt extends Component {
 state = {
     court: {},
     isEditFormDisplayed: false,
-    reDirectToHome: false,
+    reDirectToHome: false
 
 }
     componentDidMount() {
@@ -48,7 +48,52 @@ state = {
         }
 
         return (
+
+            this.state.isEditFormDisplayed
+            ?
+            <form onSubmit={this.handleSubmit}>
+                <label htmlFor='court-gym-name'>Court Name</label>
+                <input
+                type='text'
+                id='court-gym-name'
+                name='gymName'
+                onChange={this.handleInputChange}
+                value={this.state.court.gymName}
+                />
+
+                <label htmlFor='court-address'>Address:</label>
+                <input
+                type='text'
+                id='court-address'
+                name='address'
+                onChange={this.handleInputChange}
+                value={this.state.court.address}
+                />
+
+                <label htmlFor='court-number-of-players'>Players Needed:</label>
+                <input
+                type='text'
+                id='court-number-of-players'
+                name='numberOfPlayers'
+                onChange={this.handleInputChange}
+                value={this.state.court.numberOfPlayers}
+                />
+
+                <label htmlFor='entry-price'>Entry Price:</label>
+                <input
+                type='text'
+                id='court-entry-price'
+                name='entryPrice'
+                onChange={this.handleInputChange}
+                value={this.state.court.entryPrice}
+                />
+
+                <input type='submit' value='Update Court Details' />
+            </form>
+              :
             <div>
+                <button onClick={this.handleToggleEditForm}>Edit Court Details</button>
+                <button onClick={this.handleDeleteCourt}>Delete Court</button>
                 <h2> {this.state.court.gymName}</h2>
                 <h3> Address: {this.state.court.address}</h3>
                 <p>Number of Players Coming: {this.state.court.numberOfPlayers}</p>
