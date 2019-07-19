445a3329cd14b8a1d22e82c2398c6f434cf57067
JordanAclarke <JordanAclarke@gmail.com>
2019-07-19 12:10:17 -0400

    created single court

--- client/src/components/SingleCourt.js
+++ client/src/components/SingleCourt.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react'
-
+import axios from 'axios'
+import {Redirect, Link} from 'react-router-dom'
 export default class SingleCourt extends Component {
+state = {
+    court: {},
+    isEditFormDisplayed: false,
+    reDirectToHome: false,
+
+}
+    componentDidMount() {
+        axios.get(`/api/courts/${this.props.match.params.courtId}`)
+            .then((res) => {
+                this.setState({court: res.data})
+            })
+    }
     render() {
         return (
             <div>
