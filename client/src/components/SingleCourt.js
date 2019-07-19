3eed5a26b81c1c1fcc06f02cbb53eca66a35f7bd
JordanAclarke <JordanAclarke@gmail.com>
2019-07-19 13:44:53 -0400

    wrote functions for single courT

--- client/src/components/SingleCourt.js
+++ client/src/components/SingleCourt.js
@@ -14,10 +14,45 @@ state = {
                 this.setState({court: res.data})
             })
     }
+
+    handleInputChange =(event) => {
+        const copiedCourt = {...this.state.court}
+        copiedCourt[event.target.gymName] = event.target.value
+        this.setState({court: copiedCourt})
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+        axios.put(`/api/courts/${this.state.court._id}`, this.state.court)
+        .then((res) => {
+            this.setState({
+                court: res.data,
+                isEditFormDisplayed: false
+            })
+        })
+    }
+    handleToggleEditForm = () => {
+        this.setState((state) => {
+            return {isEditFormDisplayed: !state.isEditFormDisplayed}
+        })
+    }
+    handleDeleteCourt = () => {
+        axios.delete(`/api/courts/${this.state.court._id}`)
+            .then(() => {
+                this.setState({reDirectToHome: true})
+            })
+    }
     render() {
+        if(this.state.reDirectToHome) {
+            return <Redirect to='/' />
+        }
+
         return (
             <div>
-                {this.state.court.gymName}
+                <h2> {this.state.court.gymName}</h2>
+                <h3> Address: {this.state.court.address}</h3>
+                <p>Number of Players Coming: {this.state.court.numberOfPlayers}</p>
+                <p>Entry Price: {this.state.court.entryPrice}</p>
             </div>
         )
     }
