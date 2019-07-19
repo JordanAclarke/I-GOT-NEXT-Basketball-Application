20f7dc692d6d42bc0c7ad4cbb88c21026c10c7d6
JordanAclarke <JordanAclarke@gmail.com>
2019-07-19 11:00:28 -0400

    init

---
+++ client/build/precache-manifest.d67857e888f40bd38e0e0f1b77bdcc06.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c39b9c9e054477597f2d26ff3439c97",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ed9fd5a087884fbda86",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "f8d2d9344fe2689b427f",
+    "url": "/static/js/2.2f9dfdf9.chunk.js"
+  },
+  {
+    "revision": "2ed9fd5a087884fbda86",
+    "url": "/static/js/main.0b3e1fdf.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
