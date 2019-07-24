30d4b9ff86081af5d2899f00897e1491ff7e2d13
JordanAclarke <JordanAclarke@gmail.com>
2019-07-24 11:40:39 -0400

    added some homepage styling

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -5,7 +5,12 @@ export default class Home extends Component {
     render() {
         return (
             <div>
-                <h1>I G🏀T NE⛓️T </h1>
+                <h1 id='home-page'>I GOT NEXT <img src='https://previews.123rf.com/images/rastudio/rastudio1505/rastudio150502508/40451226-basketball-hoop-icon-thin-line-for-web-and-mobile-modern-minimalistic-flat-design-vector-dark-grey-i.jpg' height='97' ></img> ⛓</h1>
+            
+                <div className='wrapper'>
+                    <div className='jumping-ball'>🏀 </div>
+                    <hr></hr>
+                </div>
   <Link to='/courts'>
   <Carousel>
   <Carousel.Item>
